f7030ea972973ce2d0d0464cf3d250f0a53f5168
kuyagic <kuyagic@users.noreply.github.com>
2019-12-26 22:04:16 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/4325e34b17fb4089b645a6bfae1af43e?v=2f41a742db1942ea80fa06d0f7e3ddbd"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/32b978edabc64f07b091460a4f1f4050?v=82d8607593194d0b9e48ab9494818b96"
             }
         }
     ],
